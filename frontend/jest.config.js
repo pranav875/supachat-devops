0703b015ee9d914c747a7eb94011b807a85a17a7
Pranav <126410498+pranav875@users.noreply.github.com>
2026-04-09 16:02:09 +0530

    Fix CI/CD: Add Jest ignore patterns and clean build artifacts before tests

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -2,4 +2,6 @@ const nextJest = require('next/jest')
 const createJestConfig = nextJest({ dir: './' })
 module.exports = createJestConfig({
   testEnvironment: 'jest-environment-jsdom',
+  modulePathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/.swc/'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
 })
